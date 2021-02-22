42f5b187d6e5df4751a6d2fb0c305291d4ccf867
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2021-02-22 20:56:43 +0100

    Add text about submit mentor page

--- pages/anmal-dig-som-mentor.js
+++ pages/anmal-dig-som-mentor.js
@@ -1,4 +1,4 @@
-import { Head, Content } from '../components';
+import { Head, Content, Link } from '../components';
 
 export default function PageSubmitYouAsMentor() {
   return (
@@ -9,12 +9,49 @@ export default function PageSubmitYouAsMentor() {
         url="https://kodcoach.se/anmal-dig-som-mentor/"
       />
       <Content title="Anmäl dig som mentor">
-        <p className="pb-2">
-          En cookie är små textfiler som sparas på din dator (eller
-          mobil/läsplatta) för att förbättra din användning och upplevelse av en
-          webbplats. Svensk lag kräver att vi informerar om vilka cookies vi
-          använder, deras syfte samt hur cookies kan undvikas. Lagen säger också
-          att du som besökare måste ge samtycke till att cookies används.
+        <p className="pb-10">
+          Vad kul att du vill vara med och bidra som mentor för en eller flera
+          utvecklare.
+        </p>
+
+        <p className="pb-10">
+          Kodcoach.se drivs helt ideellt av en grupp som delar passion för
+          teknik och communityn. Vi har byggt en brygga för att hjälpa
+          utvecklare att hitta sig en mentor (kodcoach).
+        </p>
+
+        <h2 className="text-3xl font-sans bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-indigo-500 to-purple-500">
+          1. Lägg till din mentors-fil
+        </h2>
+        <p className="pb-5">
+          Börja med att forka vårt{' '}
+          <Link href="https://github.com/kodcoach/kodcoach.se">repo</Link> på
+          GitHub och kopiera "_example.yml" filen i "mentors"-mappen till en ny
+          fil i samma mapp med ett eget namn, förslagsvis döper du den till
+          samma sak som ditt GitHub-användarnamn.
+        </p>
+
+        <p className="pb-10">
+          <Link href="https://github.com/kodcoach/kodcoach.se">
+            Länk till vårt repo på GitHub
+          </Link>
+        </p>
+
+        <h2 className="text-3xl font-sans bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-indigo-500 to-purple-500">
+          2. Fyll i filen med information om dig
+        </h2>
+        <p className="pb-10">
+          I din mentors-fil så fyller du på med information om dig själv, så som
+          namn, beskrivning om dig och vilka tekniker du vill hjälpa till med.
+        </p>
+
+        <h2 className="text-3xl font-sans bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-indigo-500 to-purple-500">
+          3. Gör en pull-request
+        </h2>
+        <p className="pb-10">
+          När du är klar så gör du en pull request till vårt repo på GitHub med
+          din mentors-fil så kommer den att accepteras och kort därefter visas
+          på kodcoach.se.
         </p>
       </Content>
     </>
--- pages/hur-fungerar-det.js
+++ pages/hur-fungerar-det.js
@@ -1,5 +1,4 @@
-import { Head, Content } from '../components';
-import Link from '../components/Link';
+import { Head, Content, Link } from '../components';
 
 export default function PageHowItWorks() {
   return (
@@ -11,33 +10,45 @@ export default function PageHowItWorks() {
       />
       <Content title="Hur fungerar det?">
         <p className="pb-10">
-          Kodcoach.se drivs helt ideellt av en grupp som delar passion av teknik och communityn.
-          Vi har byggt en brygga för att hjälpa utvecklare att hitta sig en mentor (kodcoach).
+          Kodcoach.se drivs helt ideellt av en grupp som delar passion för
+          teknik och communityn. Vi har byggt en brygga för att hjälpa
+          utvecklare att hitta sig en mentor (kodcoach).
         </p>
 
-        <h2 className="text-3xl font-sans bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-indigo-500 to-purple-500">1. Kontakta en kodcoach</h2>
+        <h2 className="text-3xl font-sans bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-indigo-500 to-purple-500">
+          1. Kontakta en kodcoach
+        </h2>
         <p className="pb-10">
-          På startsidan hittar du en lista av kodcoacher som erbjuder sin hjälp.  
-          Ta kontakt med en kodcoach över mejl eller direkt via <Link href="/slack">Slack</Link>.
+          På startsidan hittar du en lista av kodcoacher som erbjuder sin hjälp.
+          Ta kontakt med en kodcoach över mejl eller direkt via{' '}
+          <Link href="/slack">Slack</Link>.
         </p>
 
-        <h2 className="text-3xl font-sans bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-indigo-500 to-purple-500">2. Kom överens om ett upplägg</h2>
+        <h2 className="text-3xl font-sans bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-indigo-500 to-purple-500">
+          2. Kom överens om ett upplägg
+        </h2>
         <p className="pb-10">
-          Du kanske behöver mycket hjälp, eller bara kunna stämma av någon gång i veckan? Ni bestämmer tillsammans!
-          Det viktiga är att ni båda är överens om en plan som passar er båda.
+          Du kanske behöver mycket hjälp, eller bara kunna stämma av någon gång
+          i veckan? Ni bestämmer tillsammans! Det viktiga är att ni båda är
+          överens om en plan som passar er båda.
         </p>
 
-        <h2 className="text-3xl font-sans bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-indigo-500 to-purple-500">3. Anslut till Slack</h2>
+        <h2 className="text-3xl font-sans bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-indigo-500 to-purple-500">
+          3. Anslut till Slack
+        </h2>
         <p className="pb-10">
-          Anslut över <Link href="/slack">Slack</Link> för att snabbt kunna hitta varandra.
+          Anslut över <Link href="/slack">Slack</Link> för att snabbt kunna
+          hitta varandra.
         </p>
 
         <p className="pb-10">
-          Vi tror att ni klarar er alldelse utmärkt själv, men vi finns alltid här om ni är osäkra på ert upplägg eller andra frågor.
+          Vi tror att ni klarar er alldelse utmärkt själv, men vi finns alltid
+          här om ni är osäkra på ert upplägg eller andra frågor.
         </p>
 
         <p className="pb-10">
-          Vi har nolltolerans mot coacher som uppför sig dåligt. Skriv till oss på <Link href="mailto:info@kodcoach.se">info@kodcoach.se</Link>!
+          Vi har nolltolerans mot coacher som uppför sig dåligt. Skriv till oss
+          på <Link href="mailto:info@kodcoach.se">info@kodcoach.se</Link>!
         </p>
       </Content>
     </>
