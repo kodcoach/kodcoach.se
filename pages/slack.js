fad01f546ab054112a0ee21c45fe709c7ca573c6
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2021-02-22 08:53:48 +0100

    Add missing space after Slack link

--- pages/slack.js
+++ pages/slack.js
@@ -6,9 +6,10 @@ export default function PageSlack() {
       <Head title="Kodcoach" description="" url="https://kodcoach.se/slack/" />
       <Content title="Slack">
         <p className="pb-2">
-          Vi använder den populära chatt tjänsten <Link href="https://slack.com/">Slack</Link> 
-          där man kan prata med sin mentor men även få hjälp från andra. Skicka
-          mailadressen ni vill använda till{' '}
+          Vi använder den populära chatt tjänsten{' '}
+          <Link href="https://slack.com/">Slack</Link> där man kan prata med sin
+          mentor men även få hjälp från andra. Skicka mailadressen ni vill
+          använda till{' '}
           <Link href="mailto:info@kodcoach.se">info@kodcoach.se</Link> så lägger
           vi till er!
         </p>
