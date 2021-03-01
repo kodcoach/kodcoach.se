a8489e2c51324f561eb3a0208a721880069b3f13
Frozzbot <fredrik.forsmo+frozzbot@gmail.com>
2021-03-01 19:31:20 +0000

    Deploy(44b36ea): Trigger Build
    Repo: kodcoach/kodcoach.se
    Workflow: Install and build
    SHA: 44b36eab20be14d5fa9927cb931a7c78dcd0fd0a
    Ref: refs/heads/develop
    Actor: frozzare

--- components/Badge/index.js
+++
@@ -1,9 +0,0 @@
-export default function Badge({ className = 'bg-gray-600 text-white', text }) {
-  return (
-    <span
-      className={`inline-flex items-center px-1.5 py-0.5 rounded text-xs font-mono ${className}`}
-    >
-      {text}
-    </span>
-  );
-}
--- components/index.js
+++
@@ -1,9 +0,0 @@
-import Badge from './Badge';
-import Content from './Content';
-import Head from './Head';
-import Icon from './Icon';
-import Layout from './Layout';
-import Link from './Link';
-import Select from './Select';
-
-export { Badge, Content, Head, Icon, Layout, Link, Select };
